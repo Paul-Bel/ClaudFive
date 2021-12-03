55187e5cea2317fcdfee992ba7ce72891f5fb86e
Paul-Bel <pn.beloborodov.msk@gmail.com>
2021-12-03 22:08:34 +0300

    Correct

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,46 +1,23 @@
 import React, {useState} from 'react';
 import s from './Сounter.module.css'
 
-type butType = {
-    but: 'i' | 'r'
+
+type nameType = "INC" | "RESET"
+
+type buttonProps = {
+    num: number,
+    buttonControl: () => void,
+    name: string
 }
 
-export function Counter() {
-    let [num, setNum] = useState(0)
-
-    const buttonControl = (but: string) => {
-        if (but === 'i' && num < 5) {
-            setNum(num + 1)
-        }
-        if (but === 'r') {
-            setNum(0)
-        }
-    }
-
+export function Button(props: buttonProps) {
     return (
-        <div className={s.counterFather}>
-            <div className={num === 5 ? s.redFive : s.counter}>{num}</div>
-
-
-            <div className={s.buttuns}> <span className={s.span && s.spanO}>
-
-                <button
-                    disabled={num === 5}
-                    className={s.button}
-                    onClick={() => buttonControl('i')}>INC</button>
-
-        </span>
-
-                <span className={s.span && s.spanT}>
-
-                    <button
-                        disabled={num === 0}
-                        className={s.button}
-                        onClick={() => buttonControl('r')}>RESET</button>
-
-                </span>
-            </div>
-        </div>
+        <span className={s.span && s.spanO}>
+        <button
+            disabled={props.name === "INC" && props.num === 5 || props.name === "RESET" && props.num === 0}
+            className={s.button}
+            onClick={props.buttonControl}>{props.name}</button>
+            </span>
     );
 }
 
--- src/Components/Claud.tsx
+++ src/Components/Claud.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import s from './Сounter.module.css'
 import {Button} from "./Button";
-import {Counters} from "./Сounters";
+import {Counters} from "./Сounter";
 
 type buttonProps = {
     num: number,
@@ -9,23 +9,21 @@ type buttonProps = {
 }
 
 export function Counter(props: buttonProps) {
+    let buttonOne = "INC"
+    let buttonTwo = "RESET"
     return (
         <div className={s.counterFather}>
+
             <Counters num={props.num}/>
             <div className={s.buttuns}>
-
                 <Button
                     num={props.num}
-                    buttonControl={() => props.buttonControl('i')}
-                    name={"INC"}/>
-
+                    buttonControl={() => props.buttonControl(buttonOne)}
+                    name={buttonOne}/>
                 <Button
                     num={props.num}
-                    buttonControl={() => props.buttonControl('r')}
-                    name={"RESET"}/>
-
-
-
+                    buttonControl={() => props.buttonControl(buttonTwo)}
+                    name={buttonTwo}/>
             </div>
         </div>
     );
