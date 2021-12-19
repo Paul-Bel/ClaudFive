9f120f8d0f0c2dcc07f45e9730e53730442be0c7
Paul-Bel <pn.beloborodov.msk@gmail.com>
2021-12-20 01:22:25 +0300

    delete input

--- src/Components/settings/SetCloud.tsx
+++ src/Components/settings/SetCloud.tsx
@@ -1,7 +1,6 @@
 import React, {ChangeEvent, useState} from "react";
 import s from './SetCloud.module.css'
 import {Button} from "../inc/Button";
-import {InputSet} from "./Input";
 
 type InputNumberType = {
     min: number,
@@ -11,35 +10,77 @@ type InputNumberType = {
     changeInc: (value: number, name: string) => void
 
 }
+const stylesInput = {'width': '30px'}
+
 
 export const SetCloud = ({min, max, ...props}: InputNumberType) => {
 
     const [setting, setSettings] = useState({value: 0, names: ''},)
     const [maxcoutn, setMaxCount] = useState<number>(max)
     const [mincoutn, setMinCount] = useState<number>(min)
+    let {nameMax, nameMin} = props.namesValue
+
+
+    const setValue = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.currentTarget.id == nameMax) {
+
+            setMaxCount(e.currentTarget.valueAsNumber)
+        }
+        if (e.currentTarget.id == nameMin) {
+            setMinCount(Number(e.currentTarget.value))
+        }
+
+        setValue(JSON.parse(e.currentTarget.value))
+    }
 
 
     const setButton = () => {
         props.changeInc(setting.value, setting.names)
     }
 
-    const setValue = (value: number, names: string) => {
-        setSettings({value, names})
-    }
+
 
     return (
         <div className={s.item}>
             <div className={s.displey}>
-            <InputSet
-                setValue={setValue}
-                min={min} max={max}
-                namesValue={props.namesValue}
-                setMaxCount={setMaxCount}
-                setMinCount={setMinCount}
-                maxcoutn={maxcoutn}
-                mincoutn={mincoutn}
-            />
+<span>   {nameMax}
+                <input
+                    style={stylesInput}
+                    min={mincoutn + 1}
+                    id={nameMax}
+                    value={maxcoutn}
+                    onChange={setValue}
+                    type="number"
+
+                />
+            </span>
+            <p/>
+
+            <span>{nameMin}
+                <input
+                    style={stylesInput}
+                    id={nameMin}
+                    value={mincoutn}
+                    onChange={setValue}
+                    type="number"
+                    min={-1}
+                    max={maxcoutn - 1}
+                />
+            </span>
+
             </div>
+
+
+            {/*<InputSet*/}
+            {/*    setValue={setValue}*/}
+            {/*    min={min} max={max}*/}
+            {/*    namesValue={props.namesValue}*/}
+            {/*    setMaxCount={setMaxCount}*/}
+            {/*    setMinCount={setMinCount}*/}
+            {/*    maxcoutn={maxcoutn}*/}
+            {/*    mincoutn={mincoutn}*/}
+            {/*/>*/}
+
 
 
             <div  className={s.button}>
